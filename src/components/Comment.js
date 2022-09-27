bce11ca40f0182ba9808569a36cea553607d5a54
Ramesh Kumar <tce.rameshkumar@gmail.com>
2022-09-27 17:57:07 +0530

    added feed URL

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,13 +1,20 @@
 
 
-function Comment({item: { id, email, body }}) {
+
+function Comment({item: { guid, title, description, permaLink, StoryImage }}) {
     return (
         <div className="col-sm-4 my-2" >
       <div className="card shadow-sm w-100" style={{ minHeight: 225 }}>
         <div className="card-body">
-          <h5 className="card-title text-center h2">Id : {id}</h5>
-          <h6 className="card-subtitle mb-2 text-muted text-center">{email}</h6>
-          <p className="card-text">{body}</p>
+          <h5 className="card-title text-left h4"> 
+          <a href={permaLink.web} className="title" target="_blank"> {title}</a>
+          </h5>
+        
+         <img className="imgfit" src={StoryImage}/>
+          <p className="card-text">{description}</p>
+          <hr/>
+          <p className="card-text text-center">Id No: {guid}</p>
+         
         </div>
       </div>
     </div>
